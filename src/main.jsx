e0f50b7cca5532a113587d2813c43249156ff386
Toluene1 <adaramoti111@gmail.com>
2023-01-30 14:17:10 +0100

    added the signup page

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./index.css";
 import GlobalProvider from "./Provider/GlobalProvider";
 import DefaultLayout from "./Layout/Defaultlayout";
+import Signup from "./Pages/Signup";
 
 const router = createBrowserRouter([
   {
@@ -12,10 +13,10 @@ const router = createBrowserRouter([
     element: <App />,
     // errorElement: <Error404 />,
   },
-  // {
-  //   path: "/signup",
-  //   element: <Signup />,
-  // },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
   // {
   //   path: "/login",
   //   element: <Login />,
