cbb0d96c3b2df24a512b8ba0edf4d3249cdf7a7a
Toluene1 <adaramoti111@gmail.com>
2023-01-30 12:32:31 +0100

    my fitst commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <section>
+      <main>
+        <h1>Tolu</h1>
+      </main>
+    </section>
+  );
+}
+
+export default App;
